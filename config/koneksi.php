cce5ce071c02dc1875b409798ee88a8b2b5784bd
Wisnu komara agung <wisnukomara89@gmail.com>
2026-05-19 17:32:58 +0700

    Push project to GitHub

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("host.docker.internal", "root", "", "dbpos_koperasi");
+$conn = mysqli_connect("db", "root", "root", "dbpos_koperasi");
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
